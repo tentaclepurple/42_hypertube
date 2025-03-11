c1ea4db5b2c1e990abf90962cf2086544b331b40
johnconh <sevenballs@gmail.com>
2025-03-11 20:14:19 +0100

    black navbar

--- frontend/app/src/app/ui/components/navbar.tsx
+++ frontend/app/src/app/ui/components/navbar.tsx
@@ -10,7 +10,7 @@ export default function Navbar() {
     const [menuOpen, setMenuOpen] = useState(false);
 
     return (
-        <nav className="bg-gray-800 p-4">
+        <nav className="bg-black-800 p-4">
             <div className="container mx-auto flex justify-between items-center">
                 <Link legacyBehavior href="/" className="text-2xl font-bold">
                     HYPERTUBE
