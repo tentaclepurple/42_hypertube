c65bf241140390cacaaa563583fac0d5061ef0a5
johnconh <sevenballs@gmail.com>
2025-04-07 17:25:46 +0200

    cambios

--- frontend/app/src/app/login/page.tsx
+++ frontend/app/src/app/login/page.tsx
@@ -57,8 +57,9 @@ export default function Login() {
         throw new Error(errorData.join(', '));
       }
       const data = await response.json();
-      login(data.access_token, data.user);
-      router.push('/'); 
+      login(data.access_token, data.user);   
+      console.log('Login successful:', data);
+      router.push('/');
     } catch (error) {
       const err = error instanceof Error ? error.message : String(error);
       setError(err);
@@ -74,99 +75,96 @@ export default function Login() {
   };
 
   return (
-    <div className="text-white max-w-screen-lg mx-auto p-6">
-      <div className="max-w-md mx-auto">
-        {loading && (
-          <div className="text-center mb-4 py-2">
-            <div className="inline-block animate-spin rounded-full h-6 w-6 border-t-2 border-white"></div>
-            <p className="mt-2">Loading...</p>
+    <div className="text-white max-w-md mx-auto p-6">
+      {loading && (
+        <div className="text-center mb-4 py-2">
+          <div className="inline-block animate-spin rounded-full h-6 w-6 border-t-2 border-white"></div>
+        </div>
+      )}
+      {error && (
+        <div className="mb-4 p-3 bg-red-100 border border-red-400 text-red-700 rounded space-y-1">
+          {error}
+        </div>
+      )}
+      <form onSubmit={handleSubmit} className="mt-6 mb-6">
+        <div className="space-y-4">
+          <div>
+            <label htmlFor='username' className="block text-sm font-medium mb-1">
+              Username
+            </label>
+            <input
+              type='text'
+              id='username'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded"
+              required
+            />
           </div>
-        )}
-        {error && (
-          <div className="mb-4 p-3 bg-red-100 border border-red-400 text-red-700 rounded space-y-1">
-            {error}
-          </div>
-        )}
-        <form onSubmit={handleSubmit} className="mt-6 mb-6">
-          <div className="space-y-4">
-            <div>
-              <label htmlFor='username' className="block text-sm font-medium mb-1">
-                Username
-              </label>
+          <div>
+            <label htmlFor='password' className="block text-sm font-medium mb-1">
+              Password
+            </label>
+            <div className="relative">
               <input
-                type='text'
-                id='username'
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
+                type={showPassword ? "text" : "password"}
+                id='password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 className="w-full p-2 border border-gray-300 rounded"
                 required
               />
-            </div>
-            <div>
-              <label htmlFor='password' className="block text-sm font-medium mb-1">
-                Password
-              </label>
-              <div className="relative">
-                <input
-                  type={showPassword ? "text" : "password"}
-                  id='password'
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  className="w-full p-2 border border-gray-300 rounded"
-                  required
-                />
-                <button 
-                  type="button" 
-                  className="absolute right-2 top-2"
-                  onClick={togglePasswordVisibility}
-                >
-                  {showPassword ? <EyeOff size={20} /> : <Eye size={20}/>}
-                </button>
-              </div>
-            </div>
-            <div className='flex items-center justify-center'>
-              <button
-                type="submit"
-                disabled={loading}
-                className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition duration-200"
+              <button 
+                type="button" 
+                className="absolute right-2 top-2"
+                onClick={togglePasswordVisibility}
               >
-                {loading ? 'Logging in...' : 'Login'}
+                {showPassword ? <EyeOff size={20} /> : <Eye size={20}/>}
               </button>
-              <p className='text-blue-600 hover:underline ml-4'>
-                Forgot your password?
-              </p>
             </div>
           </div>
-        </form>
-        
-        <div className="space-y-3 max-w-xs mx-auto mt-20"> 
-          <button
-            onClick={() => handleLogin('42')}
-            className="w-full py-2 px-4 bg-blue-600 hover:bg-blue-700 text-white rounded transition duration-200"
-          >
-            Login with 42
-          </button>
-          
-          <button
-            onClick={() => handleLogin('google')}
-            className="w-full py-2 px-4 bg-red-600 hover:bg-red-700 text-white rounded transition duration-200"
-          >
-            Login with Google
-          </button>
-          
-          <button
-            onClick={() => handleLogin('github')}
-            className="w-full py-2 px-4 bg-gray-700 hover:bg-gray-800 text-white rounded transition duration-200"
-          >
-            Login with GitHub
-          </button>
+          <div className='flex items-center justify-center'>
+            <button
+              type="submit"
+              disabled={loading}
+              className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition duration-200"
+            >
+              {loading ? 'Logging in...' : 'Login'}
+            </button>
+            <p className='text-blue-600 hover:underline ml-4'>
+              Forgot your password?
+            </p>
+          </div>
         </div>
+      </form>
+      
+      <div className="space-y-3 max-w-xs mx-auto mt-20"> 
+        <button
+          onClick={() => handleLogin('42')}
+          className="w-full py-2 px-4 bg-blue-600 hover:bg-blue-700 text-white rounded transition duration-200"
+        >
+          Login with 42
+        </button>
         
-        <div className="text-center mt-10">
-          <p className="text-gray-400">
-            Don't have an account? <Link href="/register" className="text-blue-400 hover:text-blue-300">Register here</Link>
-          </p>
-        </div>
+        <button
+          onClick={() => handleLogin('google')}
+          className="w-full py-2 px-4 bg-red-600 hover:bg-red-700 text-white rounded transition duration-200"
+        >
+          Login with Google
+        </button>
+        
+        <button
+          onClick={() => handleLogin('github')}
+          className="w-full py-2 px-4 bg-gray-700 hover:bg-gray-800 text-white rounded transition duration-200"
+        >
+          Login with GitHub
+        </button>
+      </div>
+      
+      <div className="text-center mt-10">
+        <p className="text-gray-400">
+          Don't have an account? <Link href="/register" className="text-blue-400 hover:text-blue-300">Register here</Link>
+        </p>
       </div>
     </div>
   );
--- frontend/app/src/app/register/page.tsx
+++ frontend/app/src/app/register/page.tsx
@@ -95,151 +95,149 @@ export default function Register() {
   };
 
     return (
-      <div className="p-6 bg-dark-900 text-white">
-        <div className="max-w-screen-lg mx-auto p-6">
-          {loading && (
-            <div className="text-center mb-4 py-2">
-              <div className="inline-block animate-spin rounded-full h-6 w-6 border-t-2 border-white"></div>
-              <p className="mt-2">Loading...</p>
-            </div>
-          )}
+      <div className="p-6 max-w-screen-lg mx-auto bg-dark-900 text-white">
+        {loading && (
+          <div className="text-center mb-4 py-2">
+            <div className="inline-block animate-spin rounded-full h-6 w-6 border-t-2 border-white"></div>
+            <p className="mt-2">Loading...</p>
+          </div>
+        )}
 
-          {success && (
-            <div className="text-center mt-4 py-2">
-              <div className="mb-4 p-3 bg-green-100 border border-green-400 text-green-700 rounded">
-                Registration successful! You can now <a href="/login" className="text-blue-600 hover:underline">log in</a>.
-              </div>
+        {success && (
+          <div className="text-center mt-4 py-2">
+            <div className="mb-4 p-3 bg-green-100 border border-green-400 text-green-700 rounded">
+              Registration successful! You can now <a href="/login" className="text-blue-600 hover:underline">log in</a>.
             </div>
-          )}
+          </div>
+        )}
 
-          {error.general && (
-            <div className="mb-4 p-3 bg-red-100 border border-red-400 text-red-700 rounded">
-              {error.general}
+        {error.general && (
+          <div className="mb-4 p-3 bg-red-100 border border-red-400 text-red-700 rounded">
+            {error.general}
+          </div>
+        )}
+
+        <form onSubmit={handleSubmit} className="mt-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div>
+              <label htmlFor="first_name" className="block text-sm font-medium mb-1">
+                First Name
+              </label>
+              <input
+                type="text"
+                name="first_name"
+                id="first_name"
+                value={formData.first_name}
+                onChange={handleChange}
+                className={`w-full p-2 border rounded ${error.first_name ? "border-red-500" : "border-gray-300"}`}
+                placeholder="First Name"
+              />
+              {error.first_name && <p className="text-red-500 text-sm mt-1">{error.first_name}</p>}
             </div>
-          )}
-
-          <form onSubmit={handleSubmit} className="mt-6">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              <div>
-                <label htmlFor="first_name" className="block text-sm font-medium mb-1">
-                  First Name
-                </label>
+            <div>
+              <label htmlFor="last_name" className="block text-sm font-medium mb-1">
+                Last Name
+              </label>
+              <input
+                type="text"
+                name="last_name"
+                id="last_name"
+                value={formData.last_name}
+                onChange={handleChange}
+                className={`w-full p-2 border rounded ${error.last_name ? "border-red-500" : "border-gray-300"}`}
+                placeholder="Last Name"
+              />
+              {error.last_name && <p className="text-red-500 text-sm mt-1">{error.last_name}</p>}
+            </div>
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium mb-1">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                value={formData.email}
+                onChange={handleChange}
+                className={`w-full p-2 border rounded ${error.email ? "border-red-500" : "border-gray-300"}`}
+                placeholder="you@email.com"
+              />
+              {error.email && <p className="text-red-500 text-sm mt-1">{error.email}</p>}
+            </div>
+            <div>
+              <label htmlFor="username" className="block text-sm font-medium mb-1">
+                Username
+              </label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                value={formData.username}
+                onChange={handleChange}
+                className={`w-full p-2 border rounded ${error.username ? "border-red-500" : "border-gray-300"}`}
+                placeholder="Username"
+              />
+              {error.username && <p className="text-red-500 text-sm mt-1">{error.username}</p>}
+            </div>
+            <div>
+              <label htmlFor="password" className="block text-sm font-medium mb-1">
+                Password
+              </label>
+              <div className="relative">
                 <input
-                  type="text"
-                  name="first_name"
-                  id="first_name"
-                  value={formData.first_name}
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  id="password"
+                  value={formData.password}
                   onChange={handleChange}
-                  className={`w-full p-2 border rounded ${error.first_name ? "border-red-500" : "border-gray-300"}`}
-                  placeholder="First Name"
+                  className={`w-full p-2 border rounded ${error.password ? "border-red-500" : "border-gray-300"}`}
+                  placeholder="Password minimum 8 characters"
                 />
-                {error.first_name && <p className="text-red-500 text-sm mt-1">{error.first_name}</p>}
-              </div>
-              <div>
-                <label htmlFor="last_name" className="block text-sm font-medium mb-1">
-                  Last Name
-                </label>
-                <input
-                  type="text"
-                  name="last_name"
-                  id="last_name"
-                  value={formData.last_name}
-                  onChange={handleChange}
-                  className={`w-full p-2 border rounded ${error.last_name ? "border-red-500" : "border-gray-300"}`}
-                  placeholder="Last Name"
-                />
-                {error.last_name && <p className="text-red-500 text-sm mt-1">{error.last_name}</p>}
-              </div>
-              <div>
-                <label htmlFor="email" className="block text-sm font-medium mb-1">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  className={`w-full p-2 border rounded ${error.email ? "border-red-500" : "border-gray-300"}`}
-                  placeholder="you@email.com"
-                />
-                {error.email && <p className="text-red-500 text-sm mt-1">{error.email}</p>}
-              </div>
-              <div>
-                <label htmlFor="username" className="block text-sm font-medium mb-1">
-                  Username
-                </label>
-                <input
-                  type="text"
-                  name="username"
-                  id="username"
-                  value={formData.username}
-                  onChange={handleChange}
-                  className={`w-full p-2 border rounded ${error.username ? "border-red-500" : "border-gray-300"}`}
-                  placeholder="Username"
-                />
-                {error.username && <p className="text-red-500 text-sm mt-1">{error.username}</p>}
-              </div>
-              <div>
-                <label htmlFor="password" className="block text-sm font-medium mb-1">
-                  Password
-                </label>
-                <div className="relative">
-                  <input
-                    type={showPassword ? "text" : "password"}
-                    name="password"
-                    id="password"
-                    value={formData.password}
-                    onChange={handleChange}
-                    className={`w-full p-2 border rounded ${error.password ? "border-red-500" : "border-gray-300"}`}
-                    placeholder="Password minimum 8 characters"
-                  />
-                  <button
-                    type="button"
-                    onClick={togglePasswordVisibility}
-                    className="absolute right-2 top-2"
-                  >
-                    {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
-                  </button>
-                </div>
-                {error.password && <p className="text-red-500 text-sm mt-1">{error.password}</p>}
-              </div>
-              <div>
-                <label htmlFor="confirm_password" className="block text-sm font-medium mb-1">
-                  Confirm Password
-                </label>
-                <div className="relative">
-                  <input
-                    type={showConfirmPassword ? "text" : "password"}
-                    name="confirm_password"
-                    id="confirm_password"
-                    value={formData.confirm_password}
-                    onChange={handleChange}
-                    className={`w-full p-2 border rounded ${error.confirm_password ? "border-red-500" : "border-gray-300"}`}
-                    placeholder="Confirm Password"
-                  />
-                  <button
-                    type="button"
-                    onClick={toggleConfirmPasswordVisibility}
-                    className="absolute right-2 top-2"
-                  >
-                    {showConfirmPassword ? <EyeOff size={20} /> : <Eye size={20} />}
-                  </button>
-                </div>
-                {error.confirm_password && <p className="text-red-500 text-sm mt-1">{error.confirm_password}</p>}
-              </div>
-              <div className="col-span-1 sm:col-span-2 flex justify-center">
                 <button
-                  type="submit"
-                  disabled={loading}
-                  className="w-full bg-blue-500 px-4 py-2 rounded text-white hover:bg-blue-600"
+                  type="button"
+                  onClick={togglePasswordVisibility}
+                  className="absolute right-2 top-2"
                 >
-                  {loading ? "Registering..." : "Register"}
+                  {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
                 </button>
               </div>
+              {error.password && <p className="text-red-500 text-sm mt-1">{error.password}</p>}
             </div>
-          </form>
-        </div>
+            <div>
+              <label htmlFor="confirm_password" className="block text-sm font-medium mb-1">
+                Confirm Password
+              </label>
+              <div className="relative">
+                <input
+                  type={showConfirmPassword ? "text" : "password"}
+                  name="confirm_password"
+                  id="confirm_password"
+                  value={formData.confirm_password}
+                  onChange={handleChange}
+                  className={`w-full p-2 border rounded ${error.confirm_password ? "border-red-500" : "border-gray-300"}`}
+                  placeholder="Confirm Password"
+                />
+                <button
+                  type="button"
+                  onClick={toggleConfirmPasswordVisibility}
+                  className="absolute right-2 top-2"
+                >
+                  {showConfirmPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                </button>
+              </div>
+              {error.confirm_password && <p className="text-red-500 text-sm mt-1">{error.confirm_password}</p>}
+            </div>
+            <div className="col-span-1 sm:col-span-2 flex justify-center">
+              <button
+                type="submit"
+                disabled={loading}
+                className="w-full bg-blue-500 px-4 py-2 rounded text-white hover:bg-blue-600"
+              >
+                {loading ? "Registering..." : "Register"}
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     );
   }
